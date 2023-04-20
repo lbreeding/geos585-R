aaaacb6590554f459b33722482494dc8b67aad62
lbreeding <127157740+lbreeding@users.noreply.github.com>
2023-04-20 15:21:29 -0700

    updated code

--- Geos585A/Geos585ALB.R
+++ Geos585A/Geos585ALB.R
@@ -1,3 +1,14 @@
+library(readxl)
+file_path <- "C:/Users/15203/OneDrive/Documents/GitHub/geos585-R/Geos585A/GeosTrialData.xlsx"
+data <- read_excel(file_path, sheet = "treeDataTreeRO1876R", n_max = 2)
+row_1 <- data[1, ]
+row_2 <- data[2, ]
+
+
+
+
+
+
 library(openxlsx)
 file_path <- "/OneDrive/Documents/gisclass/mgisdata/604A Applied GIS/MEKO/RCodeMeko/Geos585A/GeosTrialData.xlsx"
 xlsx_file1 <- loadWorkbook(file_path)
