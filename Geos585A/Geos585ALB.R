32b3e0cfac2ada3d72650d623adc304f9a7427d4
lbreeding <127157740+lbreeding@users.noreply.github.com>
2023-04-24 18:56:21 -0700

    Updated to be read as a matrix

--- Geos585A/Geos585ALB.R
+++ Geos585A/Geos585ALB.R
@@ -5,23 +5,38 @@ row_1 <- data[1, ]
 row_2 <- data[2, ]
 
 
+library(readxl)
+file_path <- "C:/Users/15203/OneDrive/Documents/GitHub/geos585-R/Geos585A/GeosTrialData.xlsx"
+data1 <- read_excel(file_path, sheet = "treeDataTreeRO1876R", range = "A1:XFD150")
+ids <- as.character(data1[1, ])
+units <- as.character(data1[2, ])
+ts_data <- as.matrix(data1[-c(1,2),])
 
 
 
 
-library(openxlsx)
-file_path <- "/OneDrive/Documents/gisclass/mgisdata/604A Applied GIS/MEKO/RCodeMeko/Geos585A/GeosTrialData.xlsx"
-xlsx_file1 <- loadWorkbook(file_path)
-print(xlsx_file1)
 
 
 
-sheet_names <- getSheetNames(xlsx_file1)
-first_sheet <- readWorksheet(xlsx_file, sheet = sheet_names[1])
-row1_str <- paste(first_sheet[1, ], collapse = ", ")
-row2_str <- paste(first_sheet[2, ], collapse = ", ")
-print(row1_str)
-print(row2_str)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
