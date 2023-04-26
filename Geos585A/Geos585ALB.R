b840269dcbf9b6697d153267e5ef00cdd9be6feb
lbreeding <127157740+lbreeding@users.noreply.github.com>
2023-04-25 18:00:01 -0700

    # to comment out old code

--- Geos585A/Geos585ALB.R
+++ Geos585A/Geos585ALB.R
@@ -1,8 +1,15 @@
-library(readxl)
-file_path <- "C:/Users/15203/OneDrive/Documents/GitHub/geos585-R/Geos585A/GeosTrialData.xlsx"
-data <- read_excel(file_path, sheet = "treeDataTreeRO1876R", n_max = 2)
-row_1 <- data[1, ]
-row_2 <- data[2, ]
+#library(readxl)
+#file_path <- "C:/Users/15203/OneDrive/Documents/GitHub/geos585-R/Geos585A/GeosTrialData.xlsx"
+#data <- read_excel(file_path, sheet = "treeDataTreeRO1876R", n_max = 2)
+#row_1 <- data[1, ]
+#row_2 <- data[2, ]
+
+#sheet_names <- getSheetNames(xlsx_file1)
+#first_sheet <- readWorksheet(xlsx_file, sheet = sheet_names[1])
+#row1_str <- paste(first_sheet[1, ], collapse = ", ")
+#row2_str <- paste(first_sheet[2, ], collapse = ", ")
+#print(row1_str)
+#print(row2_str)
 
 
 library(readxl)
